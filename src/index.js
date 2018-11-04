279dcdb406750835a2a40d4f806fef8e52bf8ad8
Kim Seungha <seungha.me@gmail.com>
2018-11-05 01:03:35 +0900

    babel-preset-react-app -> @babel/env

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-document.querySelector('h1').addEventListener('click', e => {
+import "@babel/polyfill"; // 이 라인을 지우지 말아주세요!
+
+document.querySelector('h1').addEventListener('click', async e => {
   alert('Hello World!');
 });
