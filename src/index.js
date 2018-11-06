77a4b4e66012be4378be577d3e57572ee507e86d
Kim Seungha <seungha.me@gmail.com>
2018-11-07 00:58:01 +0900

    시작 코드 추가

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
-import "@babel/polyfill"; // 이 라인을 지우지 말아주세요!
+import '@babel/polyfill' // 이 라인을 지우지 말아주세요!
 
-document.querySelector('h1').addEventListener('click', async e => {
-  alert('Hello World!');
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: process.env.API_URL
+})
+
+api.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers = config.headers || {}
+    config.headers['Authorization'] = 'Bearer ' + token
+  }
+  return config
 });
+
+const templates = {
+
+}
+
+const rootEl = document.querySelector('.root')
+
+// 페이지 그리는 함수 작성 순서
+// 1. 템플릿 복사
+// 2. 요소 선택
+// 3. 필요한 데이터 불러오기
+// 4. 내용 채우기
+// 5. 이벤트 리스너 등록하기
+// 6. 템플릿을 문서에 삽입
